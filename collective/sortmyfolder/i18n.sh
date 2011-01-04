6be32cb66d7485d91e7da10e5ce64ec8a8350980
markvl <markvl@localhost>
2011-01-04 10:07:02 +0000

    Add Dutch translations.
    svn path=/collective.sortmyfolder/trunk/; revision=229740

--- collective/sortmyfolder/i18n.sh
+++ collective/sortmyfolder/i18n.sh
@@ -10,7 +10,4 @@ DOMAIN2='plone'
 i18ndude rebuild-pot --pot i18n/${DOMAIN2}-temp.pot --create ${DOMAIN2} .
 i18ndude filter i18n/${DOMAIN2}-temp.pot i18n/${DOMAIN2}-exclude.pot > i18n/${DOMAIN1}-${DOMAIN2}.pot
 rm i18n/${DOMAIN2}-temp.pot
-i18ndude sync --pot i18n/${DOMAIN1}-${DOMAIN2}.pot i18n/${DOMAIN1}-${DOMAIN2}-it.po
-
-
-
+i18ndude sync --pot i18n/${DOMAIN1}-${DOMAIN2}.pot i18n/${DOMAIN1}-${DOMAIN2}-*.po
