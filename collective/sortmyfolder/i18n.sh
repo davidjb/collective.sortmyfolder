229eff681bb6ca469dba4b038deda613ec130807
Maurits van Rees <maurits@vanrees.org>
2011-11-14 11:51:46 +0000

    Translate the GS profile title and description.
    svn path=/collective.sortmyfolder/trunk/; revision=246300

--- collective/sortmyfolder/i18n.sh
+++ collective/sortmyfolder/i18n.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 DOMAIN1='collective.sortmyfolder'
-i18ndude rebuild-pot --pot locales/${DOMAIN1}.pot --create ${DOMAIN1} .
+i18ndude rebuild-pot --pot locales/${DOMAIN1}.pot --merge locales/${DOMAIN1}-manual.pot --create ${DOMAIN1} .
 i18ndude sync --pot locales/${DOMAIN1}.pot locales/*/LC_MESSAGES/${DOMAIN1}.po
 
 DOMAIN2='plone'
