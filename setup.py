a4902935f1a7d0b88a7373655f78c6027c6e940b
Maurits van Rees <maurits@vanrees.org>
2012-10-14 02:09:33 +0200

    cleanup setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ version = '1.0.3dev'
 setup(name='collective.sortmyfolder',
       version=version,
       description=("Reveal some not-so-hidden Plone feature for sorting "
-                    "your folders"),
-      long_description=open("README.txt").read() + "\n" +
-                       open("CHANGES.rst").read(),
+                   "your folders"),
+      long_description=(open("README.txt").read().strip() + "\n\n" +
+                        open("CHANGES.rst").read().strip()),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Development Status :: 5 - Production/Stable",
-        ],
+          "Framework :: Plone",
+          "Programming Language :: Python",
+          "Development Status :: 5 - Production/Stable",
+          ],
       keywords='plone folder sort',
       author='RedTurtle Technology',
       author_email='sviluppoplone@redturtle.net',
