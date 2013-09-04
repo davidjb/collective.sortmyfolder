966997dc23ef90ce679c90c50770b65bd22bac79
Maurits van Rees <maurits@vanrees.org>
2013-09-04 18:57:45 +0200

    Do not show the option to sort on position if getObjPositionInParent is not available.

--- collective/sortmyfolder/browser/sort_contents.py
+++ collective/sortmyfolder/browser/sort_contents.py
@@ -12,6 +12,13 @@ class SortContentsView(BrowserView):
     def __init__(self, context, request):
         self.context = context
         self.request = request
+        # Watch out: if the restrictedTraverse fails and there is no
+        # fallback, the action will not show up in the actions drop
+        # down and you will see no error or warning about it.
+        if context.restrictedTraverse('getObjPositionInParent', False):
+            self.use_position_script = True
+        else:
+            self.use_position_script = False
         self.request.set('disable_border', True)
 
     def __call__(self, *args, **kw):
